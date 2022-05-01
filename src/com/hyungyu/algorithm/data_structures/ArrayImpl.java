17c3a8b324c831c0a552653084beb6cc12908f5e
hyungyu <yhg8484@naver.com>
2022-05-01 21:41:37 +0900

    [algorithm][자료구조] 피드백 반영

--- src/com/hyungyu/algorithm/data_structures/ArrayImpl.java
+++ src/com/hyungyu/algorithm/data_structures/ArrayImpl.java
@@ -10,8 +10,10 @@ public class ArrayImpl implements Array {
             throw new IllegalArgumentException("The length of sNum must be equal to N.");
         }
         char[] chars = sNum.toCharArray();
+        // 반복문 돌 때 배열에 계속 접근하기 보다 변수에 할당해서 쓰는게 좋음.
+        int length = chars.length;
         int sum = 0;
-        for (int i = 0; i < chars.length; i++) {
+        for (int i = 0; i < length; i++) {
             sum += chars[i] - '0';
             // 이렇게도 쓸 수 있다.
             // sum += Character.getNumericValue(chars[i]);
@@ -30,7 +32,9 @@ public class ArrayImpl implements Array {
         if (testScores.length != N) {
             throw new IllegalArgumentException("The length of testScores must be equal to N.");
         }
-        double sum = 0;
+
+        // sum 은 구해지는 값이 정수형이기 때문에 int 로 선언
+        int sum = 0;
         int max = 0;
         for (int i = 0; i < testScores.length; i++) {
             if (max < testScores[i]) {
@@ -40,6 +44,6 @@ public class ArrayImpl implements Array {
         }
 
         // TODO testScores 의 모든 값이 0 일 경우는?
-        return sum * 100 / max / N;
+        return sum * 100.0 / max / N;
     }
 }
