3d5e69d0f91261bd853975579c13828dbbe0d757
hyungyu <yhg8484@naver.com>
2022-05-13 00:24:57 +0900

    [algorithm][자료구조] 스택과 큐 4

---
+++ src/com/hyungyu/algorithm/book/stack_and_queue/P11268_절대값힙.java
@@ -0,0 +1,35 @@
+package com.hyungyu.algorithm.book.stack_and_queue;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class P11268_절대값힙 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> queue = new PriorityQueue<>((o1, o2) -> {
+           int first_abs = Math.abs(o1);
+           int second_abs = Math.abs(o2);
+           if (first_abs == second_abs) {
+               return o1 > o2 ? 1 : -1;
+           }
+           return first_abs - second_abs;
+        });
+
+        for (int i = 0; i < N; i++) {
+            int request = Integer.parseInt(br.readLine());
+            if (request == 0) {
+                if (queue.isEmpty()) {
+                    System.out.println("0");
+                } else {
+                    System.out.println(queue.poll());
+                }
+            } else {
+                queue.add(request);
+            }
+        }
+    }
+}
