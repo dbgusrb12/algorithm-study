86d1637a55efd667491dacc836842d70bbce5e45
hyungyu <yhg8484@naver.com>
2022-06-06 19:46:37 +0900

    [algorithm][정수론] 유클리드 호제법 3

---
+++ src/com/hyungyu/algorithm/book/number_theory/P1033_칵테일.java
@@ -0,0 +1,96 @@
+package com.hyungyu.algorithm.book.number_theory;
+
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class P1033_칵테일 {
+    public static ArrayList<cNode>[] A;
+    public static long lcm;
+    public static boolean[] visitArray;
+    public static long[] D;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        A = new ArrayList[N];
+        visitArray = new boolean[N];
+        D = new long[N];
+        lcm = 1;
+        for (int i = 0; i < N; i++) {
+            A[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < N - 1; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int p = sc.nextInt();
+            int q = sc.nextInt();
+            A[a].add(new cNode(b, p, q));
+            A[b].add(new cNode(a, p, q));
+            lcm *= (p * q / gcd(p, q));
+        }
+        D[0] = lcm;
+        DFS(0);
+        long mgcd = D[0];
+        for (int i = 1; i < N; i++) {
+            mgcd = gcd(mgcd, D[i]);
+        }
+        for (int i = 0; i < N; i++) {
+            System.out.println(D[i] / mgcd + " ");
+        }
+    }
+
+    public static long gcd(long a, long b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+
+    public static void DFS(int node) {
+        visitArray[node] = true;
+        for (cNode i : A[node]) {
+            int next = i.getB();
+            if (!visitArray[next]) {
+                D[next] = D[node] * i.getQ() / i.getP();
+                DFS(next);
+            }
+        }
+    }
+
+    public static class cNode {
+        private int b;
+        private int p;
+        private int q;
+        public cNode(int b, int p, int q) {
+            super();
+            this.b = b;
+            this.p = p;
+            this.q = q;
+        }
+
+        public int getB() {
+            return b;
+        }
+
+        public void setB(int b) {
+            this.b = b;
+        }
+
+        public int getP() {
+            return p;
+        }
+
+        public void setP(int p) {
+            this.p = p;
+        }
+
+        public int getQ() {
+            return q;
+        }
+
+        public void setQ(int q) {
+            this.q = q;
+        }
+    }
+}
