c0d088b3d3a56044c06518ecca76487fb749a64f
hyungyu <yhg8484@naver.com>
2022-06-28 21:41:56 +0900

    [algorithm][트리] 세그먼트 트리 3

--- src/com/hyungyu/algorithm/book/tree/P10868_최솟값.java
+++ src/com/hyungyu/algorithm/book/tree/P10868_최솟값.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class P2042_구간합구하기 {
+public class P10868_최솟값 {
 
     static long[] tree;
 
@@ -14,10 +14,8 @@ public class P2042_구간합구하기 {
         StringTokenizer st = new StringTokenizer(br.readLine());
         // 수의 개수
         int N = Integer.parseInt(st.nextToken());
-        // 수의 변경이 일어나는 횟수
+        // 구간의 최솟값을 구하는 횟수
         int M = Integer.parseInt(st.nextToken());
-        // 구간의 합을 구하는 횟수
-        int K = Integer.parseInt(st.nextToken());
         int length = N;
         // 트리의 높이 선언 및 초기화
         int treeHeight = 0;
@@ -29,33 +27,25 @@ public class P2042_구간합구하기 {
         int treeSize = (int) Math.pow(2, treeHeight + 1);
         // 리프 노드 시작 인덱스 추출
         int leafNodeStartIndex = treeSize / 2 - 1;
+
         tree = new long[treeSize + 1];
-        // 리프 노드 세팅
+        int treeLength = tree.length;
+        for (int i = 0; i < treeLength; i++) {
+            tree[i] = Integer.MAX_VALUE;
+        }
         for (int i = leafNodeStartIndex + 1; i <= leafNodeStartIndex + N; i++) {
             tree[i] = Long.parseLong(br.readLine());
         }
         // 초기 트리 세팅
         setTree(treeSize - 1);
 
-        for (int i = 0; i < M + K; i++) {
+        for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            // 1 = start 번째 수를 end 로 변경, 2 = start 번째 수 부터 end 번째 수 까지의 구간 합
-            long a = Integer.parseInt(st.nextToken());
-            int startIndex = Integer.parseInt(st.nextToken());
-            long endValue = Long.parseLong(st.nextToken());
-            if (a == 1) {
-                // startIndex 번째 수를 endValue 로 변경
-                changeValue(leafNodeStartIndex + startIndex, endValue);
-                continue;
-            }
-            if (a == 2) {
-                // start 번째 수 부터 end 번째 수 까지의 구간 합
-                startIndex += leafNodeStartIndex;
-                endValue += leafNodeStartIndex;
-                System.out.println(getSum(startIndex, (int) endValue));
-                continue;
-            }
-            break;
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            start += leafNodeStartIndex;
+            end += leafNodeStartIndex;
+            System.out.println(getMin(start, end));
         }
     }
 
@@ -66,48 +56,32 @@ public class P2042_구간합구하기 {
      */
     public static void setTree(int index) {
         while (index != 1) {
-            tree[index / 2] += tree[index];
+            tree[index / 2] = Math.min(tree[index / 2], tree[index]);
             index--;
         }
     }
 
     /**
-     * index 번째 수를 customValue 로 변경
-     *
-     * @param index : 변경 할 index
-     * @param customValue : 변경 할 수
-     */
-    public static void changeValue(int index, long customValue) {
-        long diff = customValue - tree[index];
-        while (index > 0) {
-            tree[index] += diff;
-            index /= 2;
-        }
-    }
-
-    /**
-     * 구간 합 구하기
+     * 최솟값 구하기
      *
      * @param startIndex : 시작 index
-     * @param endIndex : 끝 index
-     * @return 구간 합
+     * @param endIndex   : 끝 index
+     * @return 최솟값
      */
-    public static long getSum(int startIndex, int endIndex) {
-        long sum = 0;
+    public static long getMin(int startIndex, int endIndex) {
+        long min = Long.MAX_VALUE;
         while (startIndex <= endIndex) {
             if (startIndex % 2 == 1) {
-                sum += tree[startIndex];
+                min = Math.min(min, tree[startIndex]);
                 startIndex++;
             }
             if (endIndex % 2 == 0) {
-                sum += tree[endIndex];
+                min = Math.min(min, tree[endIndex]);
                 endIndex--;
             }
             startIndex /= 2;
             endIndex /= 2;
         }
-
-        return sum;
+        return min;
     }
-
 }
