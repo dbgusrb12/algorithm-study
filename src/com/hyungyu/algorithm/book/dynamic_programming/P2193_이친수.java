614ba44d628c85cb6316ef7d5cdfb91d528247c9
hyungyu <yhg8484@naver.com>
2022-07-12 12:03:29 +0900

    [algorithm][동적계획법] 동적 계획법 3

---
+++ src/com/hyungyu/algorithm/book/dynamic_programming/P2193_이친수.java
@@ -0,0 +1,22 @@
+package com.hyungyu.algorithm.book.dynamic_programming;
+
+import java.util.Scanner;
+
+public class P2193_이친수 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        long[][] D = new long[N + 1][2];
+        // 1자리 이친수는 1만 존재한다.
+        D[1][1] = 1;
+        D[1][0] = 0;
+
+        for (int i = 2; i <= N; i++) {
+            D[i][0] = D[i - 1][1] + D[i - 1][0];
+            D[i][1] = D[i - 1][0];
+        }
+        System.out.println(D[N][0] + D[N][1]);
+    }
+
+}
