09d127716584fd68e3d2a9c922d8029efeee25b0
hyungyu <yhg8484@naver.com>
2022-10-23 17:13:28 +0900

    [algorithm][leetcode] First Bad Version

--- src/com/hyungyu/algorithm/leetcode/search/P704_BinarySearch.java
+++ src/com/hyungyu/algorithm/leetcode/search/P704_BinarySearch.java
@@ -3,7 +3,7 @@ package com.hyungyu.algorithm.leetcode.search;
 public class P704_BinarySearch {
 
     public static void main(String[] args) {
-        int[] nums = {-1,0,3,5,9,12};
+        int[] nums = {-1, 0, 3, 5, 9, 12};
         int target = 9;
         int answer = search(nums, target);
         System.out.println(answer);
