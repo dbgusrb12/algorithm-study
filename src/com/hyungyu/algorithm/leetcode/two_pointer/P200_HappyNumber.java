38163b8bc53e1ba72e7e38ca5802cfb892f05f88
hyungyu <yhg8484@naver.com>
2022-11-13 18:01:23 +0900

    [algorithm][leetcode] Happy Number

---
+++ src/com/hyungyu/algorithm/leetcode/two_pointer/P200_HappyNumber.java
@@ -0,0 +1,29 @@
+package com.hyungyu.algorithm.leetcode.two_pointer;
+
+public class P200_HappyNumber {
+
+    public static void main(String[] args) {
+        boolean answer = isHappy(2);
+        System.out.println(answer);
+    }
+
+    public static int getNext(int n) {
+        int totalSum = 0;
+        while (n > 0) {
+            int d = n % 10;
+            n = n / 10;
+            totalSum += d * d;
+        }
+        return totalSum;
+    }
+
+    public static boolean isHappy(int n) {
+        int slowRunner = n;
+        int fastRunner = getNext(n);
+        while (fastRunner != 1 && slowRunner != fastRunner) {
+            slowRunner = getNext(slowRunner);
+            fastRunner = getNext(getNext(fastRunner));
+        }
+        return fastRunner == 1;
+    }
+}
