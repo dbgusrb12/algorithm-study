e4aa4a15086162bf4539ca4821fde76cb9f28fea
hyungyu <yhg8484@naver.com>
2023-02-05 15:42:13 +0900

    [algorithm][leetcode] Remove Element

---
+++ src/com/hyungyu/algorithm/leetcode/two_pointer/P27_RemoveElement.java
@@ -0,0 +1,21 @@
+package com.hyungyu.algorithm.leetcode.two_pointer;
+
+public class P27_RemoveElement {
+
+    public static void main(String[] args) {
+        int[] nums = {0,1,2,2,3,0,4,2};
+        int val = 2;
+        int k = removeElement(nums, val);
+        System.out.println(k);
+    }
+
+    public static int removeElement(int[] nums, int val) {
+        int index = 0;
+        for (int num : nums) {
+            if (num != val) {
+                nums[index++] = num;
+            }
+        }
+        return index;
+    }
+}
